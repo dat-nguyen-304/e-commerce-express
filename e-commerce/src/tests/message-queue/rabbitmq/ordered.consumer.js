0e15f83f027a7f8e25c5ad4f0af2ca289cf1565a
dat-nguyen-304 <nguyenphatdat3004@gmail.com>
2024-08-08 23:36:13 +0700

    handle ordered queue message

---
+++ e-commerce/src/tests/message-queue/rabbitmq/ordered.consumer.js
@@ -0,0 +1,23 @@
+const amqp = require('amqplib');
+
+async function consumerOrderedMessage() {
+  const connection = await amqp.connect('amqp://localhost');
+
+  const channel = await connection.createChannel();
+  const queueName = 'ordered-queued-message';
+  await channel.assertQueue(queueName, {
+    durable: true,
+  });
+
+  channel.prefetch(1);
+
+  channel.consume(queueName, (msg) => {
+    const message = msg.content.toString();
+    setTimeout(() => {
+      console.log('processed: ', message);
+      channel.ack(msg);
+    }, Math.random() * 1000);
+  });
+}
+
+consumerOrderedMessage().catch(console.error);
